4004d2c07141e14f9160d32cd3d23889b9afd8c4
Eddie <edwardchauke@hotmail.com>
2026-07-15 11:09:39 +0200

    Configure Cloudinary media storage

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,9 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "website",
+
+    "cloudinary",
+    "cloudinary_storage",
 ]
 
 # ======================================================
@@ -176,3 +179,13 @@ MEDIA_ROOT = BASE_DIR / "media"
 # ======================================================
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = (
+    "cloudinary_storage.storage.MediaCloudinaryStorage"
+)
