d0fb977b33b2094cb6b09decdcc3d84516f7af64
Eddie <edwardchauke@hotmail.com>
2026-07-17 09:35:16 +0200

    Add gallery page template

--- website/models.py
+++ website/models.py
@@ -17,7 +17,6 @@ class Service(models.Model):
         auto_now_add=True
     )
 
-
     def __str__(self):
         return self.name
 
@@ -49,18 +48,33 @@ class Project(models.Model):
         auto_now_add=True
     )
 
-
     def __str__(self):
         return self.title
-    
-    from django.db import models
+
+
 
 class Gallery(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    image = models.ImageField(upload_to="gallery/")
-    category = models.CharField(max_length=50)
-    completed_date = models.DateField(null=True, blank=True)
+
+    title = models.CharField(
+        max_length=100
+    )
+
+    description = models.TextField(
+        blank=True
+    )
+
+    image = models.ImageField(
+        upload_to="gallery/"
+    )
+
+    category = models.CharField(
+        max_length=50
+    )
+
+    completed_date = models.DateField(
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.title
